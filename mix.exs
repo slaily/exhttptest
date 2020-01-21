9a253375acfb69d0bfa62fa76404669fad82c8b2
slaily <slavov.iliyan96@gmail.com>
2020-01-21 17:38:30 +0200

    Create an executable

--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Exhttptest.MixProject do
+defmodule ExHTTPTest.MixProject do
   use Mix.Project
 
   def project do
@@ -7,6 +7,7 @@ defmodule Exhttptest.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      escript: escript_config,
       deps: deps()
     ]
   end
@@ -24,5 +25,9 @@ defmodule Exhttptest.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp escript_config do
+    [main_module: CLI]
   end
 end
