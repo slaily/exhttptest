04b8a3a97d5533d27a57612fab8c5ba334d1aef6
slaily <slavov.iliyan96@gmail.com>
2020-01-22 13:44:07 +0200

    Implement a utility function that reads a file

--- lib/exhttptest/utils.ex
+++ lib/exhttptest/utils.ex
@@ -2,4 +2,11 @@ defmodule ExHTTPTest.Utils do
   def file_ext_supported?(file) do
     Path.extname(file) in [".json"]
   end
+
+  def read_file(file) do
+    case file_ext_supported?(file) do
+      true -> File.read(file)
+      false -> {:unsupported, :file_ext}
+    end
+  end
 end
