9aab86c2e5afad3aebd4221336f7202b3ed3a827
slaily <slavov.iliyan96@gmail.com>
2020-01-24 16:30:27 +0200

    Add a test case for functionality that validates content under specific conditions

--- test/core_test.exs
+++ test/core_test.exs
@@ -8,4 +8,18 @@ defmodule CoreTest do
 
     assert {:ok, true} == {response, is_map(content)}
   end
+
+  test "Does the content is valid under specific conditions?" do
+    content = %{
+      "endpoint" => "users",
+      "headers" => %{"Accept-Language" => "en-US"},
+      "host" => "http://localhost:8080",
+      "name" => "TEST: List all users",
+      "query_string" => %{"limit" => 5},
+      "verb" => "GET"
+    }
+    {response, _} = Core.validate_content({:ok, content})
+
+    assert response == :valid
+  end
 end
